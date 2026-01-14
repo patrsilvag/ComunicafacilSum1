6994a82d3012b38f25de8497e31db7f813ab0ba3
patrsilvag <patr.silvag@duocuc.cl>
2026-01-14 16:14:40 -0300

    Inputs de correo y contraseña en Registro

--- app/src/main/java/com/psilva/comunicafacil/ui/screens/RegisterScreen.kt
+++ app/src/main/java/com/psilva/comunicafacil/ui/screens/RegisterScreen.kt
@@ -1,15 +1,25 @@
 package com.psilva.comunicafacil.ui.screens
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
-
 
 @Composable
 fun RegisterScreen() {
+
+    // Variables de estado para los inputs
+    var correo by remember { mutableStateOf("") }
+    var clave by remember { mutableStateOf("") }
 
     val estadoSnackbar = remember { SnackbarHostState() }
 
@@ -29,6 +39,36 @@ fun RegisterScreen() {
                 style = MaterialTheme.typography.headlineMedium,
                 modifier = Modifier.padding(bottom = 16.dp)
             )
+
+            // Correo Electrónico
+            OutlinedTextField(
+                value = correo,
+                onValueChange = { correo = it },
+                label = { Text("Correo electrónico") },
+                modifier = Modifier.fillMaxWidth(),
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Email, // Teclado con @
+                    imeAction = ImeAction.Next // Botón Siguiente
+                )
+            )
+
+            Spacer(modifier = Modifier.height(12.dp))
+
+            //  Contraseña
+            OutlinedTextField(
+                value = clave,
+                onValueChange = { clave = it },
+                label = { Text("Contraseña") },
+                modifier = Modifier.fillMaxWidth(),
+                singleLine = true,
+                visualTransformation = PasswordVisualTransformation(), // Ocultar caracteres
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Password,
+                    imeAction = ImeAction.Done // Botón ok
+                )
+            )
+
 
         }
     }
