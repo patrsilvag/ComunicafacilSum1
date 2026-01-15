c7cfa5e4d78d123802780cfabb3ef070065250dc
patrsilvag <patr.silvag@duocuc.cl>
2026-01-15 11:43:58 -0300

    Agregar NavGraph con rutas Login, Registro y Recuperar

---
+++ app/src/main/java/com/psilva/comunicafacil/navigation/NavGraph.kt
@@ -0,0 +1,38 @@
+package com.psilva.comunicafacil.navigation
+
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.psilva.comunicafacil.ui.screens.LoginScreen
+import com.psilva.comunicafacil.ui.screens.RecoverScreen
+import com.psilva.comunicafacil.ui.screens.RegisterScreen
+
+@Composable
+fun NavGraph(navController: NavHostController) {
+
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Login.route
+    ) {
+        composable(Screen.Login.route) {
+            LoginScreen(
+                onIrARegiclearstro = { navController.navigate(Screen.Registro.route) },
+                onIrARecuperar = { navController.navigate(Screen.Recuperar.route) }
+            )
+        }
+
+        composable(Screen.Registro.route) {
+            RegisterScreen(
+                onVolverLogin = { navController.popBackStack() }
+            )
+        }
+
+        composable(Screen.Recuperar.route) {
+            RecoverScreen(
+                onVolverLogin = { navController.popBackStack() }
+            )
+        }
+    }
+}
