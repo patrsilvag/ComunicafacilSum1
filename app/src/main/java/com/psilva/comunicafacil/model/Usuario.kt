b372493cd66002a1a9c4f33cf6b18dfd5b9e6153
patrsilvag <patr.silvag@duocuc.cl>
2026-01-15 14:02:17 -0300

    Agregar modelo Usuario

---
+++ app/src/main/java/com/psilva/comunicafacil/model/Usuario.kt
@@ -0,0 +1,9 @@
+package com.psilva.comunicafacil.model
+
+data class Usuario(
+    val correo: String,
+    val clave: String,
+    val tipoUsuario: String,
+    val aceptaTerminos: Boolean,
+    val preferencia: String
+)
