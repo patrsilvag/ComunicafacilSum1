c1797321a86d38bdbd28f1b4212d15ae0ecd11dd
patrsilvag <patr.silvag@duocuc.cl>
2026-01-15 11:32:41 -0300

    Definir rutas de navegación

---
+++ app/src/main/java/com/psilva/comunicafacil/navigation/Screen.kt
@@ -0,0 +1,7 @@
+package com.psilva.comunicafacil.navigation
+
+sealed class Screen(val route: String) {
+    data object Login : Screen("login")
+    data object Registro : Screen("registro")
+    data object Recuperar : Screen("recuperar")
+}
