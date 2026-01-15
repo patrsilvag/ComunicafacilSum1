6c9d50333308bf2ee181b97405725b6026743e1b
patrsilvag <patr.silvag@duocuc.cl>
2026-01-15 10:41:43 -0300

    Recover se agrega campo correo

--- app/src/main/java/com/psilva/comunicafacil/ui/screens/RecoverScreen.kt
+++ app/src/main/java/com/psilva/comunicafacil/ui/screens/RecoverScreen.kt
@@ -1,23 +1,44 @@
 package com.psilva.comunicafacil.ui.screens
 
-
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun RecoverScreen() {
+
+    var correo by remember { mutableStateOf("") }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(16.dp)
     ) {
+
         Text(
             text = "Recuperar contraseña",
             style = MaterialTheme.typography.headlineMedium
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        OutlinedTextField(
+            value = correo,
+            onValueChange = { correo = it },
+            label = { Text("Correo electrónico") },
+            modifier = Modifier.fillMaxWidth(),
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Email,
+                imeAction = ImeAction.Done
+            )
         )
     }
 }
